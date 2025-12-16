6b0276fa6ef1b8ed23451176c97be460210f5cd1
ARTHUR <kosty@ARTHUR>
2025-12-16 17:40:35 +0300

    new program just cout?

--- First_Console/First_Console.cpp
+++ First_Console/First_Console.cpp
@@ -4,44 +4,11 @@
 
 int main()
 {
-    int choiseAction;
-    cout << "Select an action: " << endl;
-    cin >> choiseAction;
+    string player;
+    cout << "Enter a something..? " << endl;
+    cin >> player;
 
-    int firstValue = 0, secondValue = 0, sum = 0;
-
-    cout << "Enter a first Value: " << endl;
-    cin >> firstValue;
-    cout << "Enter a second Value: " << endl;
-    cin >> secondValue;
-
-    switch (choiseAction)
-    {
-    case 1:
-        sum = firstValue + secondValue;
-        cout << "Result: " << sum << endl;
-        break;
-    case 2:
-        sum = firstValue - secondValue;
-        cout << "Result: " << sum << endl;
-        break;
-    case 3:
-        sum = firstValue * secondValue;
-        cout << "Result: " << sum << endl;
-        break;
-    case 4:
-        if (secondValue == 0) {
-            cout << "Error: Division by zero is not allowed." << endl;
-        }
-        else {
-            sum = firstValue / secondValue;
-            cout << "Result: " << sum << endl;
-        }
-        break;
-    default:
-        cout << "Invalid choice!" << endl;
-        break;
-    }
+    cout << player << endl;
 
 
     return 0;
