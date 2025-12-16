7206193890162d2eebef3ef266dfa310caf0d5dc
ARTHUR <kosty@ARTHUR>
2025-12-16 17:42:53 +0300

    Upgraded

--- First_Console/First_Console.cpp
+++ First_Console/First_Console.cpp
@@ -5,10 +5,12 @@
 int main()
 {
     string player;
+    int choise = 0;
     cout << "Enter a something..? " << endl;
     cin >> player;
 
-    cout << player << endl;
+    cout << "Right? " << player << endl;
+    cout << "If: yes - enter 1. no - enter 2." << endl;
 
 
     return 0;
