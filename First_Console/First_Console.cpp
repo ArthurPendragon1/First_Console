6cc9103c54238c2a4e518c3167dfdbcedff6577d
ARTHUR <kosty@ARTHUR>
2025-12-31 21:47:51 +0300

    Center of Estimate

--- First_Console/First_Console.cpp
+++ First_Console/First_Console.cpp
@@ -4,21 +4,36 @@
 
 int main()
 {
-    int a = 0;
+    float firstValue = 0.0, secondValue = 0.0, threeValue = 0.0, fourValue = 0.0, fiveValue = 0.0;
+    float avgValue;
+    
+        cout << "Enter a Value: " << endl;
+        cin >> firstValue;
+        if (firstValue < 0 || firstValue > 5)
+            return 1;
+        cout << "Enter a Value: " << endl;
+        cin >> secondValue;
+        if (secondValue < 0 || secondValue > 5)
+            return 1;
+        cout << "Enter a Value: " << endl;
+        cin >> threeValue;
+        if (threeValue < 0 || threeValue > 5)
+            return 1;
+        cout << "Enter a Value: " << endl;
+        cin >> fourValue;
+        if (fourValue < 0 || fourValue > 5)
+            return 1;
+        cout << "Enter a Value: " << endl;
+        cin >> fiveValue;
+        if (fiveValue < 0 || fiveValue > 5)
+            return 1;
 
-    switch (a)
-    {
-        case 0:
-            while (a < 1000)
-            a++;
-            cout << a << endl;
-            if (a == 1000)
-            {
-                cout << "Value is: " << a << endl;
-                a++;
-            }
-            break;
-    }
+        avgValue = (firstValue + secondValue + threeValue + fourValue + fiveValue) / 5.0;
+
+        cout << "The current estimate is: " << avgValue << endl;
+    
+
+    
 
     return 0;
 }
